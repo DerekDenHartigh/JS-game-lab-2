1256bc5210812b5f0482643ada333825994084a5
Derek <derekdenhartigh@gmail.com>
2019-02-14 21:43:57 -0500

    game finished for part 1, part 2 still to come

--- js-game-lab-2.js
+++ js-game-lab-2.js
@@ -5,37 +5,32 @@ function startGame(){ // The game, you just lost it.
     if (gameAnswer==="yes"||gameAnswer==="ya"||gameAnswer==="yeah"||gameAnswer==="yeet"||gameAnswer==="yup"||gameAnswer==="uhuh"||gameAnswer==="affirmative"||gameAnswer==="fo sho"||gameAnswer==="you know it"){ // only yes is working, I'm not sure why.
         let userName = prompt("What name shall I have them inscribe upon your epitaph?")
         if (userName==="") {
-            let userName2="";
-            while (userName2===("")){
+            while (userName===("")){
             console.log("Answer ME! I am Grant the mighty");
-            userName2 = prompt("TELL ME YOUR NAME!!!"); 
-            // combat code dupe here, will need to change userName's to userName2's
+            userName = prompt("TELL ME YOUR NAME!!!"); 
             }
         }
-        // startCombat(); just plugging the function in until i learn hoisting and such
-        // let userHP = 40; want to get userHP from 
         let gameDifficulty = prompt("Select your difficulty level:\n\tEasy\n\tMedium\n\tHard\n\tImpossible").toLowerCase();
+        let userHP = 40;
         switch(gameDifficulty){
-            case "easy": let userHP = 100; break;
-            case "medium": let userHP = 40; break;
-            case "hard": let userHP = 20; break;
-            case "impossible": let userHP = 0; break;
-            default: console.log("ERROR"); break;} // so this should define User's HP? but its local to the switch... how to get userHP out
+            case "easy": userHP = 100; break;
+            case "medium": userHP = 40; break;
+            case "hard": userHP = 20; break;
+            case "impossible": userHP = 1; break;
+            default: console.log("ERROR"); break;}
         let grantHP = 10;
         let userWins = 0;
         let grantDefeats = 0;  // Fear Grant the undefeated
-        while (userHP>0 && grantDefeats<3){
+        while (userHP>0 && userWins<3){
             userHP-=Math.floor((Math.random()*2)+1);
             grantHP-=Math.floor((Math.random()*2)+1);
             console.log(`Grant swings at ${userName},\n\t${userName}'s HP = ${userHP}`);
             console.log(`${userName} swings at Grant,\n\tGrant's HP = ${grantHP}`);
             if (grantHP<=0){
-                console.log("I am REBORN!!!\n\t -Grant")
-                grantHP = 10; // will this reset grantHP?
+                console.log("I am REBORN!!!\n\t-Grant")
+                grantHP = 10;
                 userWins++;
-                grantDefeats++;
             }
-    
         }
         if (userWins>=3) {console.log(`Just kidding!\n*Final Death Rattle* \n\t -Grant \n${userName} is WINNER!  Grant the Mighty is Defeat`)}
         else {console.log(`HAHAHAHAHA you ${userName} are defeat, I am champion still!`)};
@@ -61,7 +56,7 @@ function startCombat(){
         }
 
     }
-    if (userWins>=3) {console.log(`*Final Death Rattle* \n\t -Grant \n${userName} is WINNER!  Grant the Mighty is Defeat`)}
+    if (userWins>=3) {console.log(`*Final Death Rattle* \n\t -Grant \n${userName} is WINNER!  Grant the Mighty is Defeat`)} // Gramatic inspiration: https://en.wikipedia.org/wiki/Big_Rigs:_Over_the_Road_Racing
     else {console.log(`HAHAHAHAHA you ${userName} are defeat, I am champion still!`)}
 }
 
